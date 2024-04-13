643b9589a60dbb41c4bd4b70d6862e4cebb69396
armoredpigeon <mjonesartand3d@gmail.com>
2024-04-12 21:00:10 -0600

    no more autoIncrement

--- helloWorld/src/main/java/com/mysandbox/helloworld/GreetingController.java
+++ helloWorld/src/main/java/com/mysandbox/helloworld/GreetingController.java
@@ -1,6 +1,7 @@
 package com.mysandbox.helloworld;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.concurrent.atomic.AtomicLong;
@@ -22,22 +23,24 @@ public class GreetingController {
 
     @GetMapping("/greet-all")
     public List<Greeting> greetAll(@RequestParam(value="name", defaultValue="Everybody") String name){
-        List<Greeting> greetingsList = Stream.of(
-            "Shawn",
-            "Gus",
-            "Jules",
-            "Lassy",
-            "Woody",
-            "Chief Vic",
-            "McNab",
-            "Mr Yin",
-            "Mr Yang",
-            "Mary",
-            "Henry",
-            name
-        )
-        .map((String item) -> {
-            return new Greeting(counter.incrementAndGet(), String.format(item=="Henry" ? derogatoryTemplate : template, item));
+        List<Greeting> greetingsList = Map.ofEntries(
+            Map.entry(7L, "Shawn"),
+            Map.entry(54L, "Gus"),
+            Map.entry(34L, "Jules"),
+            Map.entry(48L, "Lassy"),
+            Map.entry(32L, "Woody"),
+            Map.entry(42L, "Chief Vic"),
+            Map.entry(16L, "McNab"),
+            Map.entry(64L, "Mr Yin"),
+            Map.entry(128L, "Mr Yang"),
+            Map.entry(256L, "Mary"),
+            Map.entry(512L, "Henry"),
+            Map.entry(1024L, name)
+        ).entrySet().stream()
+        .map((Map.Entry item) -> {
+            var givenKey = (long) item.getKey();
+            var givenVal = item.getValue();
+            return new Greeting(givenKey, String.format(givenVal=="Henry" ? derogatoryTemplate : template, givenVal));
         })
         .collect(Collectors.toList());
         return greetingsList;
