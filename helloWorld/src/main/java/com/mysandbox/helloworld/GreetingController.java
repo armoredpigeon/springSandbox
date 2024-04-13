1ba89ea3f7ebe2ab1ff090dd47d9c4f072fa3667
armoredpigeon <mjonesartand3d@gmail.com>
2024-04-12 19:20:54 -0600

    added greet-all endpoint

--- helloWorld/src/main/java/com/mysandbox/helloworld/GreetingController.java
+++ helloWorld/src/main/java/com/mysandbox/helloworld/GreetingController.java
@@ -15,4 +15,27 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+
+    @GetMapping("/greet-all")
+    public Greeting[] greetAll(@RequestParam(value="name", defaultValue="Everybody") String name){
+        String[] namesList = {
+            "Shawn",
+            "Gus",
+            "Jules",
+            "Lassy",
+            "Woody",
+            "Chief Vic",
+            "McNab",
+            "Mr Yin",
+            "Mr Yang",
+            "Mary"
+        };
+        Greeting[] greetingsList = new Greeting[namesList.length + 1];
+        for (int i = 0; i<namesList.length; i++){
+            Greeting tempGreeting = new Greeting(counter.incrementAndGet(), String.format(template, namesList[i]));
+            greetingsList[i] = tempGreeting;
+        }
+        greetingsList[namesList.length] = new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return greetingsList;
+    }
 }
