f04c64406f9b4a69c9f400f1f355bd1640b0d652
armoredpigeon <mjonesartand3d@gmail.com>
2024-04-14 12:15:26 -0600

    Adding the post to add a new greeting

--- helloWorld/src/main/java/com/mysandbox/helloworld/GreetingController.java
+++ helloWorld/src/main/java/com/mysandbox/helloworld/GreetingController.java
@@ -7,6 +7,8 @@ import java.util.stream.Stream;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,27 +18,38 @@ public class GreetingController {
     private static final String derogatoryTemplate = "Suck it, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    private List<Greeting> greetingsList = List.of(
+        new Greeting(7L, String.format(template,"Shawn")),
+        new Greeting(54L, String.format(template, "Gus")),
+        new Greeting(34L, String.format(template, "Jules")),
+        new Greeting(48L, String.format(template, "Lassy")),
+        new Greeting(32L, String.format(template, "Woody")),
+        new Greeting(42L, String.format(template, "Chief Vic")),
+        new Greeting(16L, String.format(template, "McNab")),
+        new Greeting(64L, String.format(template, "Mr Yin")),
+        new Greeting(128L, String.format(template, "Mr Yang")),
+        new Greeting(256L, String.format(template, "Mary")),
+        new Greeting(512L, String.format(derogatoryTemplate, "Henry"))
+    );
+
+    public void AddGreeting(Greeting greeting){
+        greetingsList.add(greeting);
+    }
+
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
     @GetMapping("/greet-all")
-    public List<Greeting> greetAll(@RequestParam(value="name", defaultValue="Everybody") String name){
-        List<Greeting> greetingsList = List.of(
-            new Greeting(7L, String.format(template,"Shawn")),
-           new Greeting(54L, String.format(template, "Gus")),
-           new Greeting(34L, String.format(template, "Jules")),
-           new Greeting(48L, String.format(template, "Lassy")),
-           new Greeting(32L, String.format(template, "Woody")),
-           new Greeting(42L, String.format(template, "Chief Vic")),
-           new Greeting(16L, String.format(template, "McNab")),
-           new Greeting(64L, String.format(template, "Mr Yin")),
-           new Greeting(128L, String.format(template, "Mr Yang")),
-           new Greeting(256L, String.format(template, "Mary")),
-           new Greeting(512L, String.format(derogatoryTemplate, "Henry")),
-           new Greeting(1024L, String.format(template, name))
-        );
+    public List<Greeting> greetAll(){
         return greetingsList;
+    }
+
+    @PostMapping("/add-greeting")
+    public ResultData addGreeting(@RequestBody Greeting newGreeting){
+        greetingsList.add(newGreeting);
+
+        return new ResultData(200);
     }
 }
---
+++ helloWorld/src/main/java/com/mysandbox/helloworld/ResultData.java
@@ -0,0 +1,3 @@
+package com.mysandbox.helloworld;
+
+public record ResultData(int status){}
